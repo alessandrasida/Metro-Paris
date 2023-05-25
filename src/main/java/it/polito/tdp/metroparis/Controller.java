9ad882312ad96d0a691782525390a6797c2f9e97
aless <aless@172.21.41.37>
2023-05-25 11:37:43 +0200

    .

--- src/main/java/it/polito/tdp/metroparis/Controller.java
+++ src/main/java/it/polito/tdp/metroparis/Controller.java
@@ -1,14 +1,19 @@
 package it.polito.tdp.metroparis;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.metroparis.model.Fermata;
+import it.polito.tdp.metroparis.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 
 public class Controller {
-
+	
+	private Model model;
     @FXML
     private ResourceBundle resources;
 
@@ -16,22 +21,37 @@ public class Controller {
     private URL location;
 
     @FXML
-    private ComboBox<?> boxArrivo;
+    private ComboBox<Fermata> boxArrivo;
 
     @FXML
-    private ComboBox<?> boxPartenza;
+    private ComboBox<Fermata> boxPartenza;
 
     @FXML
     private TextArea txtResult;
 
     @FXML
     void handleCalcola(ActionEvent event) {
-
+    	Fermata partenza = this.boxPartenza.getValue();
+    	Fermata arrivo = this.boxArrivo.getValue();
+    	
+    	if(partenza != null && arrivo != null && !partenza.equals(arrivo)) {
+    		List<Fermata> percorso = model.percorso(partenza, arrivo);
+    		txtResult.setText("Percorso tra " + partenza.getNome() + " e " + arrivo.getNome()+ "\n");
+    		for( Fermata f : percorso) {
+    			this.txtResult.appendText(f.getNome() +"\n");
+    			
+    		}
+    	}else {
+    		this.txtResult.appendText("Devi selezionare due stazioni diverse tra loro.\n");
+    	}
     }
 
     @FXML
     void handleCrea(ActionEvent event) {
-
+    	this.model.creaGrafo();
+    	if( this.model.isGrafoLoaded()) {
+    		this.txtResult.setText("Grafo correttamente importato.");
+    	}
     }
 
     @FXML
@@ -41,5 +61,12 @@ public class Controller {
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Metro.fxml'.";
 
     }
+
+	public void setModel(Model m) {
+		this.model = m;
+		List<Fermata> fermate = this.model.getAllFermate();
+		this.boxPartenza.getItems().setAll(fermate);
+		this.boxArrivo.getItems().setAll(fermate);
+	}
 
 }
--- src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
+++ src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
@@ -6,10 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.javadocmd.simplelatlng.LatLng;
 
 import it.polito.tdp.metroparis.model.Connessione;
+import it.polito.tdp.metroparis.model.Coppie;
 import it.polito.tdp.metroparis.model.Fermata;
 import it.polito.tdp.metroparis.model.Linea;
 
@@ -26,7 +28,7 @@ public class MetroDAO {
 			ResultSet rs = st.executeQuery();
 
 			while (rs.next()) {
-				Fermata f = new Fermata(rs.getInt("id_Fermata"), rs.getString("nome"),
+				Fermata f = new Fermata(rs.getInt("id_fermata"), rs.getString("nome"),
 						new LatLng(rs.getDouble("coordx"), rs.getDouble("coordy")));
 				fermate.add(f);
 			}
@@ -68,6 +70,156 @@ public class MetroDAO {
 
 		return linee;
 	}
+
+	public boolean isConnesse(Fermata partenza, Fermata arrivo) {
+		
+		String sql = "SELECT  COUNT(*) AS c "
+				+ "FROM connessione "
+				+ "WHERE id_stazP=? "
+				+ "AND id_stazA=? ";
+		
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			st.setInt(2, arrivo.getIdFermata());
+			
+			ResultSet res = st.executeQuery();
+			
+			res.first();
+			int c = res.getInt("c");
+			
+			
+			conn.close();
+			
+			return c!=0;
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		
+	}
+
+	public List<Fermata> trovaCollegate(Fermata partenza) {
+		
+		String sql = "SELECT * "
+				+ "from fermata "
+				+ "where id_fermata IN ( "
+				+ "SELECT id_stazA "
+				+ "FROM connessione "
+				+ "WHERE id_stazP=? "
+				+ "GROUP BY id_stazA "
+				+ ") "
+				+ "ORDER BY nome ASC ";
+		
+		List<Fermata>	fermate = new ArrayList<>();
+		
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			
+			ResultSet rs = st.executeQuery();
+			
+			while( rs.next()) {
+				Fermata f = new Fermata( rs.getInt("id_fermata"), rs.getString("nome"),
+						new LatLng(rs.getDouble("coordx"), rs.getDouble("coordy")));
+				fermate.add(f);
+				
+						
+			}
+			
+			
+			st.close();
+			conn.close();
+
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return fermate;
+	}
+
+	
+	
+public List<Fermata> trovaIdCollegate(Fermata partenza, Map<Integer, Fermata> fermataIdMap) {
+		
+		String sql = "SELECT id_stazA "
+				+ "from connessione  "
+				+ "where id_stazP = ? "
+				+ "GROUP BY id_stazA ";
+		
+		List<Fermata>	fermate = new ArrayList<>();
+		
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			
+			ResultSet rs = st.executeQuery();
+			
+			while( rs.next()) {
+				Integer idFermata = rs.getInt("id_stazA");
+				fermate.add(fermataIdMap.get(idFermata));		
+			}
+			
+			
+			st.close();
+			conn.close();
+
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return fermate;
+	}
+
+
+
+	public List<Coppie> getAllCoppie( Map<Integer, Fermata> FermateIdMap){
+		
+		String sql = "SELECT distinct id_stazP, id_stazA "
+				+ "FROM connessione ";
+		
+		List<Coppie> allCoppie = new ArrayList<>();
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				Coppie coppia = new Coppie(FermateIdMap.get(rs.getInt("id_stazP")), 
+						FermateIdMap.get(rs.getInt("id_StazA")));
+				allCoppie.add(coppia);
+			}
+			
+			
+			st.close();
+			conn.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return allCoppie;
+	}
+
+
 
 	
 
